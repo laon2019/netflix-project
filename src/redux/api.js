f2985702dccee105a56f0f01ce3181214a6e05cb
laon2019 <laon5987@nate.com>
2023-07-25 01:43:36 +0900

    1

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https:api.themoviedb.org/3",
+    baseURL: "https://api.themoviedb.org/3",
     headers: {"Content-Type": "application/json"}
 })
+
 
 api.interceptors.request.use(function (config){
     console.log("request start", config)
